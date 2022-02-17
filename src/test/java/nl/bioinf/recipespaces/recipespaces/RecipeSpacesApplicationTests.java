7ecc17c3efae609d013218a9872e7a033f41395c
JurrienDeJong <ju.de.jong@st.hanze.nl>
2022-02-17 13:33:53 +0100

    First Commit

---
+++ src/test/java/nl/bioinf/recipespaces/recipespaces/RecipeSpacesApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bioinf.recipespaces.recipespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
