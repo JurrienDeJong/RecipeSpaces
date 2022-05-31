17a237a1a85d0edff6deb17039f25bc83ae16a68
Rose Hazenberg <rosehazenberg@gmail.com>
2022-05-31 13:09:26 +0200

    added html logger formatter

---
+++ src/main/java/nl/bioinf/recipespaces/logging/HtmlLogFormatter.java
@@ -0,0 +1,26 @@
+package nl.bioinf.recipespaces.logging;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class HtmlLogFormatter extends Formatter {
+    public String format(LogRecord record) {
+        return ("<tr><td>"
+                + (new Date(record.getMillis()))
+                + "</td><td>"
+                + record.getMessage()
+                + "</td></tr>\n");
+    }
+
+    public String getHead(Handler h) {
+        return ("<html>\n <body>\n"
+                + "<Table border>\n<tr>"
+                + "<td>Time</td><td>Log Message</td></tr>\n");
+    }
+
+    public String getTail(Handler h) {
+        return ("</table>\n</body>\n</html>");
+    }
+}
