9d89ac5e66c33c2e40bc8cc404d9db31e8af4c26
jorick44 <jorickbaron@gmail.com>
2022-06-13 12:05:57 +0200

    updated toolbar to make JSON files and objects for later use at backend

---
+++ src/main/java/nl/bioinf/recipespaces/controller/RatioController.java
@@ -0,0 +1,45 @@
+package nl.bioinf.recipespaces.controller;
+
+import com.google.gson.Gson;
+import nl.bioinf.recipespaces.model.Ingredient;
+import nl.bioinf.recipespaces.model.IngredientAmount;
+import nl.bioinf.recipespaces.model.Recipe;
+import nl.bioinf.recipespaces.service.IngredientAmountService;
+import nl.bioinf.recipespaces.service.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
+import java.util.logging.Level;
+
+/**
+ * Handles view of ratio
+ * Author: Jorick Baron
+ */
+@Controller()
+@RequestMapping(path = "ratio")
+public class RatioController {
+
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(RatioController.class.getName());
+
+    private final IngredientAmountService ingredientAmountService;
+    private final RecipeService recipeService;
+
+    public RatioController(IngredientAmountService ingredientAmountService, RecipeService recipeService) {
+        this.ingredientAmountService = ingredientAmountService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/{id}")
+    public String showRatio(Model model, @PathVariable("id") Integer id) {
+        logger.log(Level.INFO, "The ratio page of the recipe with id: " + id);
+        recipeService.parseRecipe(model, id, recipeService, ingredientAmountService);
+
+        logger.log(Level.INFO, "Returning ratio page with convertible ingredient amounts for recipe with id: " + id);
+        return "ratios";
+    }
+
+}
--- src/main/java/nl/bioinf/recipespaces/controller/RecipeSpaceController.java
+++ src/main/java/nl/bioinf/recipespaces/controller/RecipeSpaceController.java
@@ -2,16 +2,19 @@ package nl.bioinf.recipespaces.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import nl.bioinf.recipespaces.model.Link;
-import nl.bioinf.recipespaces.model.Node;
-import nl.bioinf.recipespaces.model.RecipeSpace;
+import nl.bioinf.recipespaces.model.*;
+import nl.bioinf.recipespaces.service.IngredientAmountService;
+import nl.bioinf.recipespaces.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -26,36 +29,37 @@ public class RecipeSpaceController {
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(RecipeSpaceController.class.getName());
 
     private static FileWriter file;
+    private final IngredientAmountService ingredientAmountService;
+    private final RecipeService recipeService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String spaces() throws IOException {
+    public RecipeSpaceController(IngredientAmountService ingredientAmountService, RecipeService recipeService) {
+        this.ingredientAmountService = ingredientAmountService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/{id}")
+    public String spaces(Model model, @PathVariable("id") Integer id) throws IOException {
         logger.log(Level.INFO, "Serving the page with the visualization of recipe spaces");
-        createVisualisation();
+        HashMap<String, String> recipeMap = recipeService.parseRecipe(model, id, recipeService, ingredientAmountService);
+
+        createVisualisation(recipeMap);
         return "recipeSpaceView";
     }
 
-    private void createVisualisation() throws IOException {
+    private void createVisualisation(HashMap<String, String> recipeMap) throws IOException {
         logger.log(Level.INFO, "Creating visualization");
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        Node n1 = new Node("node1", 1);
-        Node n2 = new Node("node2", 2);
-        Node n3 = new Node("node3", 3);
-
-        Link l1 = new Link(n1.id(), n2.id(), 50);
-        Link l2 = new Link(n1.id(), n3.id(), 200);
-        Link l3 = new Link(n2.id(), n3.id(), 500);
-
         List<Node> nodes = new ArrayList<>();
-        nodes.add(n1);
-        nodes.add(n2);
-        nodes.add(n3);
-
         List<Link> links = new ArrayList<>();
-        links.add(l1);
-        links.add(l2);
-        links.add(l3);
+
+        for (String ingredient : recipeMap.keySet()) {
+            nodes.add(new Node(ingredient, 1));
+        }
+
+        for (Node node : nodes) {
+            links.add(new Link(node.id(), nodes.get(1).id(), 100));
+        }
 
         RecipeSpace recipeSpace = new RecipeSpace(nodes, links);
 
@@ -64,7 +68,7 @@ public class RecipeSpaceController {
 
     private void writeToFile(Gson gson, RecipeSpace recipeSpace) {
         try {
-            FileWriter myWriter = new FileWriter("src/main/resources/static/json/recipespace.json");
+            FileWriter myWriter = new FileWriter("src/main/resources/static/json/recipespace_original.json");
             myWriter.write(gson.toJson(recipeSpace));
             myWriter.close();
             logger.log(Level.INFO, "Creating json when without error's");
